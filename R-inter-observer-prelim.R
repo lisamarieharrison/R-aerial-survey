518a2e42b1bcc63afa250fc336fb86227c9784ca
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-04-08 16:09:47 +1000

    Changed to omit double counting.

--- R-inter-observer-prelim.R
+++ R-inter-observer-prelim.R
@@ -21,40 +21,42 @@ checkSame <- function(r1, r2) {
 
 
 
-findOverlap <- function(set1, set2, i, j, overlap, lisa_missed, vic_missed) {
+findOverlap <- function(set1, set2, i, j,overlap, lisa_missed, vic_missed) {
   
-
-  set2 <- set2[set2$Date == set1$Date[i], ]
   
   if (j > nrow(set2)) {
     print(paste(i, "No overlapping observation found. Try next observation"))
-    lisa_missed <- rbind(lisa_missed, c(as.character(set1$Date[i]), as.character(set1$Species[i])))
+    lisa_missed <- rbind(lisa_missed, set1[i, ])
     return(list(overlap = overlap, vic_missed = vic_missed, lisa_missed = lisa_missed))
   }
   
   if (checkSame(set1[i, ], set2[j, ])) {
-    rbind(overlap, set1[i, ])
+    overlap <- rbind(overlap, set1[i, ])
     print("Success: found a same observation!")
-    vic_missed <- vic_missed[-which(vic_missed$Date == set1$Date[i] & substr(vic_missed$Time, 1, 5) == substr(set1$Time[i], 1, 5) & vic_missed$Species == set1$Species[i]), ]
+    vic_missed <- vic_missed[-which(vic_missed$Date == set1$Date[i] & substr(vic_missed$Time, 1, 5) == substr(set1$Time[i], 1, 5) & vic_missed$Species == set1$Species[i])[1], ]
     return(list(overlap = overlap, vic_missed = vic_missed, lisa_missed = lisa_missed))
   } else {
-    findOverlap(set1, set2, i = i, j = j + 1)
+    findOverlap(set1, set2, i = i, j = j + 1, overlap = overlap, lisa_missed = lisa_missed, 
+                vic_missed = vic_missed)
   }
-    
+  
 }
 
 overlap <- dat[0, ]
 vic_missed <- lisa
-lisa_missed <- matrix(0, ncol = 2, nrow = 1)
+lisa_missed <- dat[0, ]
 
 for (i in 1:nrow(vic)) {
-  result <- findOverlap(set1 = vic, set2 = vic_missed, i = i, j = 1, 
+  set2 <- vic_missed[vic_missed$Date == vic$Date[i], ]
+  
+  result <- findOverlap(set1 = vic, set2 = set2, i = i, j = 1, 
                         overlap = overlap, lisa_missed = lisa_missed, 
                         vic_missed = vic_missed)
   overlap <- result$overlap
   lisa_missed <- result$lisa_missed
   vic_missed <- result$vic_missed
 }
+lisa_missed <- lisa_missed[-1, ]
 
 
 
