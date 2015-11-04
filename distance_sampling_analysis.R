35464b0770209ce0830774ce5427a41f2e0c9a91
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-11-04 16:35:18 +1100

    Removing functions and putting them in a separate folder for sourcing

--- distance_sampling_analysis.R
+++ distance_sampling_analysis.R
@@ -16,6 +16,14 @@ library(RUnit)
 #source mrds code modified to use only single sided strip width
 source("C:/Users/Lisa/Documents/phd/aerial survey/R/code/R-aerial-survey/mrds_modified_functions.R")
 
+#source other required functions
+source("C:/Users/Lisa/Documents/phd/aerial survey/R/code/R-aerial-survey/functions/calcAbundanceSingle.R")
+source("C:/Users/Lisa/Documents/phd/aerial survey/R/code/R-aerial-survey/functions/sightingsAtLevel.R")
+source("C:/Users/Lisa/Documents/phd/aerial survey/R/code/R-aerial-survey/functions/createDistanceData.R")
+source("C:/Users/Lisa/Documents/phd/aerial survey/R/code/R-aerial-survey/functions/corObsByPercent.R")
+source("C:/Users/Lisa/Documents/phd/aerial survey/R/code/R-aerial-survey/functions/calcEnvtEffort.R")
+
+
 #merge lisa and vic's observations to get all sightings
 all_obs <- rbind(lisa_obs, vic_obs)
 table(all_obs$Flight.Direction, all_obs$Species)
@@ -29,89 +37,17 @@ vic_obs$Species[vic_obs$Species %in% c("BS", "W", "Wh", "S")] <- "S"
 
 dat.south <- rbind(lisa_obs[lisa_obs$Flight.Direction == "S", ], vic_obs[vic_obs$Flight.Direction == "S", ])
 
-for (t in unique(vic_obs$Trial)) {
-  for (s in c("B", "BOT", "S")) {
-    cor_factor <- percent_of_lisa[which(names(percent_of_lisa) == s)]
-    n_rows <- nrow(vic_obs[vic_obs$Trial == t & vic_obs$Species == s, ])
-    if (n_rows != 0) {
-      n_rep <- abs(n_rows - round(n_rows / cor_factor))
-      rep_row <- vic_obs[vic_obs$Trial == t & vic_obs$Species == s, ][1, ]
-      rep_row$Dist..from.transect <- mean(na.omit(vic_obs[vic_obs$Trial == t & vic_obs$Species == s, ]$Dist..from.transect))
-      for (i in 1:n_rep) {
-        vic_obs <- rbind(vic_obs, rep_row)
-      }
-    }
-  }
-}
+#correct vics number of sightings using percentage of obs seen by lisa
+vic_obs <- corObsByPercent(dat = vic_obs, percent_mat = percent_of_lisa) 
 
+#combine lisa and vics observations to get all corrected observations
 cor_obs <- rbind(lisa_obs, vic_obs)
 
 #---------------------------- DETECTION FUNCTIONS -----------------------------#
 
-
-
-createData <- function(species, lisa_obs, direction, truncate=NULL) {
-  
-  #creates data frame in the form mrds requires for distance sampling
-  #species = character with the species code. e.g.: "BOT" = bottlenose dolphin
-  #lisa_obs = data.frame of all observations
-  #direction = character with flight direction. "N" = north, "S" = south
-  #truncate = optional numeric specifying the distance (m) from transect to truncate sightings
-  #return = data.frame with columns object, observer, detected, distance, species, Trial, size
-  
-  lisa_obs <- lisa_obs[lisa_obs$Flight.Direction == direction & 
-                         lisa_obs$Type == "S" & 
-                         lisa_obs$Dist..from.transect !=0 & 
-                         !is.na(lisa_obs$Dist..from.transect) & 
-                         lisa_obs$Species == species, ]
-
-  if (!is.null(truncate)) {
-    lisa_obs <- lisa_obs[lisa_obs$Dist..from.transect <= truncate, ]
-  }
-  
-  total_observations <- cbind(1:nrow(lisa_obs), rep(1, nrow(lisa_obs)), rep(1, nrow(lisa_obs)), lisa_obs$Dist..from.transect, lisa_obs$Species, lisa_obs$Trial, lisa_obs$Number)
-  total_observations <- data.frame(total_observations)
-  colnames(total_observations) <- c("object", "observer", "detected", "distance", "species", "Trial", "size")
-
-  total_observations <- apply(total_observations, 2, as.character)
-  total_observations <- data.frame(apply(total_observations, 2, as.numeric))
-  
-  if (species == "BOT") {
-    total_observations <- total_observations[!is.na(total_observations$size), ]
-  } else {
-    total_observations <- total_observations[, 1:6]
-  }
-  
-  return(total_observations)
-  
-}
-
-calcAbundanceSingle <- function(area, dataframe, model) {
-  
-  #calculates density and abundance using distance sampling data
-  #area = survey area (km2). Wollongong - Newcastle = 265km
-  #dataframe = total_observations data frame from distanceData function
-  #model = ddf model
-  
-  obs.table <- cbind(rep(1, nrow(dataframe)), dataframe$Trial, dataframe)
-  colnames(obs.table)[1:2] <- c("Region.Label", "Sample.Label")
-  
-  region.table <- data.frame(matrix(c(1, area), ncol = 2, byrow = T))
-  colnames(region.table) <- c("Region.Label", "Area")
-  
-  sample.table <- data.frame(cbind(rep(1, 54), c(1:54), rep(265, 54)))
-  colnames(sample.table) <- c("Region.Label", "Sample.Label", "Effort")
-  
-  d <- dht(model, region.table = region.table, sample.table = sample.table, obs.table = obs.table, 
-           options = list(convert.units = 0.001))
-  
-  return(d)
-  
-}
-
 strip_width <- 1000
 
-total_observations <- createData(species = "S", cor_obs, truncate = strip_width, direction = "S")
+total_observations <- createDistanceData(species = "B", cor_obs, truncate = strip_width, direction = "S")
 
 p_total <- ddf(method = 'ds',dsmodel =~ cds(key = "gamma", formula=~1), 
                data = total_observations, meta.data = list(left = 50, width = strip_width))
@@ -128,7 +64,7 @@ for (s in c("B", "BOT", "S")) {
   
   for (d in c("N", "S")) {
     
-    total_observations <- createData(species = s, cor_obs, truncate = strip_width, direction = d)
+    total_observations <- createDistanceData(species = s, cor_obs, truncate = strip_width, direction = d)
     
     p_total <- ddf(method = 'ds',dsmodel =~ cds(key = "gamma", formula=~1), 
                    data = total_observations, meta.data = list(left = 50, width = 300))
@@ -151,95 +87,10 @@ for (s in c("B", "BOT", "S")) {
 #-------------------------- Environmental variables ---------------------------#
 
 #remove single "LT" or "RT" or times will be incorrect
-dat.south <- dat.south[!dat.south$Time == "", ]
-dat.south[, c("Beaufort.Sea.State", "Water.clarity")] <- apply(dat.south[, c("Beaufort.Sea.State", "Water.clarity")], 2, as.integer)
-test <- dat.south$Type[dat.south$Type %in% c("LT", "RT")]
-for (i in 2:length(test)) {
-  if ((test[i] == "LT" & test[i - 1] == "LT") | (test[i] == "RT" & test[i - 1] == "RT")) {
-    dat.south <- dat.south[-i, ]
-  }
-}
+dat.south <- correctLTRT(dat.south)
 
-#initialize environmental variables
-envt.var.south <- matrix(0, ncol = 27)
-colnames(envt.var.south) <- c(
-  "Beaufort.Sea.State.south.1",
-  "Beaufort.Sea.State.south.2",
-  "Beaufort.Sea.State.south.3",
-  "Beaufort.Sea.State.south.4", 
-  "Cloud.cover.south.0",
-  "Cloud.cover.south.1",
-  "Cloud.cover.south.2",
-  "Cloud.cover.south.3",
-  "Cloud.cover.south.4",
-  "Cloud.cover.south.5",
-  "Cloud.cover.south.6",
-  "Cloud.cover.south.7",
-  "Cloud.cover.south.8",
-  "Water.clarity.south.1",
-  "Water.clarity.south.2",
-  "Water.clarity.south.3",
-  "Glare.south.0",
-  "Glare.south.10",
-  "Glare.south.30",
-  "Glare.south.15",
-  "Glare.south.25",
-  "Glare.south.5",
-  "Glare.south.20",
-  "Glare.south.35",
-  "Glare.south.50",
-  "Glare.south.60",
-  "Glare.south.100"
-)
 
-#sighting effort for southbound data
-for (i in unique(dat.south$Date)) {
-  for (j in 1:(nrow(dat.south[dat.south$Date == i, ]) - 1)) {
-    
-    if (dat.south[dat.south$Date == i, ]$Type[j] != "LT") {
-      
-      
-      time.start <- chron(times. = as.character(dat.south$Time[dat.south$Date == i][j]), format = "h:m:s")
-      time.stop  <- chron(times. = as.character(dat.south$Time[dat.south$Date == i][j + 1]), format = "h:m:s")
-      mins       <- hours(time.stop - time.start)*60 + minutes(time.stop - time.start)
-      
-      if (mins > 30) {
-        print(c(i, mins)) #check for times that have been typed in incorrectly
-      }
-      
-      #add minute differences to each environmental level
-      for (k in c(24, 27, 28, 30)) {
-        w <- which(colnames(envt.var.south) == as.name(paste(names(dat.south)[k], ".south.", dat.south[dat.south$Date == i, ][j, k], sep = "")))
-        envt.var.south[1, w] <- envt.var.south[1, w] + mins
-      }
-    }
-  }
-}
-
-sightingsAtLevel <- function(environmenal_hrs, dat, env_variable, species, flight_direction) {
-  
-  #calculates the number of sightings per hour at each environmental condition level
-  #environmental_hrs = vector of hours at each level. e.g.: envt.var.south
-  #dat = full data set of observations. e.g.: lisa_obs
-  #env_variable = character containing the name of the environmental variable
-  #species = character containing the species code. e.g.: "B" = baitfish
-  #flight_direction = character containing the flight direction. "S" = south, "N" = north
-  
-  hrs <- environmenal_hrs[grep(env_variable, colnames(environmenal_hrs))]/60
-  t <- table(factor(dat[dat$Species == species & dat$Flight.Direction == flight_direction, which(colnames(dat) == env_variable)],
-                    levels = unique(dat[dat$Flight.Direction == flight_direction, 
-                                        which(colnames(dat) == env_variable)])))
-  t <- t[order(names(t))]                                                                                                                                 
-  sightings_hr <- matrix(t/hrs, ncol = length(t))
-  colnames(sightings_hr) <- names(t)
-  
-  #species hash table
-  species_full <- cbind(c("B", "BOT", "S"), c("Baitfish", "Dolphins", "Sharks"))
-  Species <- species_full[which(species_full[, 1] == species), 2]
-  
-  kable(sightings_hr, format = "pandoc", caption = paste("Number of sightings per hour effort at each", env_variable, "for", Species))
-  
-}
+envt.var.south <- calcEnvtEffort(dat.south)
 
 #sea state table
 sightingsAtLevel(environmenal_hrs = envt.var.south, dat = dat.south, env_variable = "Beaufort.Sea.State", species = "S", flight_direction = "S")
