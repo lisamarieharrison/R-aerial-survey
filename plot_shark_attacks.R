a2ba0ff5ade9ff0bb850c5e999cf36357e05a257
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-03-22 17:07:21 +1100

    Map attack locations around australia by species for Sam

---
+++ plot_shark_attacks.R
@@ -0,0 +1,24 @@
+setwd("C:/Users/43439535/Documents/Lisa/phd/aerial survey/data/speed trials")
+
+lisa  <- read.csv("speed_trials_datasheet_lisa.csv", header = T, skip = 1)
+
+library(mapdata)
+library(maps)
+library(maptools)
+library(ggplot2)
+library(plotly)
+
+aust  <- map("world2Hires", regions="Australia", fill = TRUE, col = "grey")
+aust_poly <- map2SpatialPolygons(aust, IDs = aust$names, proj4string=CRS("+proj=longlat +datum=WGS84"))
+aust_ggplot   <- fortify(aust_poly, region="id") #df for ggplot
+
+
+p <- ggplot() + 
+  geom_polygon(data=aust_ggplot, aes(x=long, y=lat, group=group), color="black", fill = "grey") +
+  geom_point(aes(lisa_points[, 1], lisa_points[, 2], colour = lisa_labels),
+            position=position_jitter(h=0, w=0)) 
+ggplotly(p)
+
+
+
+
