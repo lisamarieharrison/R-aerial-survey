a521d3462fbf2086f60a87f63633fe28818dd6d0
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-03-22 17:17:43 +1100

    Changed limits of plot to zoom around Australia

--- plot_shark_attacks.R
+++ plot_shark_attacks.R
@@ -8,17 +8,15 @@ library(maptools)
 library(ggplot2)
 library(plotly)
 
-aust  <- map("world2Hires", regions="Australia", fill = TRUE, col = "grey")
+aust  <- map("world2Hires", regions="Australia", fill = TRUE, col = "grey", xlim = c(110, 160), ylim = c(-45, -10.05140))
 aust_poly <- map2SpatialPolygons(aust, IDs = aust$names, proj4string=CRS("+proj=longlat +datum=WGS84"))
 aust_ggplot   <- fortify(aust_poly, region="id") #df for ggplot
 
 
 p <- ggplot() + 
   geom_polygon(data=aust_ggplot, aes(x=long, y=lat, group=group), color="black", fill = "grey") +
-  geom_point(aes(lisa_points[, 1], lisa_points[, 2], colour = lisa_labels),
+  geom_point(aes(lisa_points[, 1], lisa_points[, 2], shape = lisa_labels, colour = lisa_labels),
             position=position_jitter(h=0, w=0)) 
 ggplotly(p)
-
-
 
 
