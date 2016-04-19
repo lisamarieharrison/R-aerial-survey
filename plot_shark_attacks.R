3a3e4a4021fbee1ffeb67d073e08c95e7588e90f
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-04-19 17:00:25 +1000

    Not sure of changes

--- plot_shark_attacks.R
+++ plot_shark_attacks.R
@@ -3,7 +3,12 @@
 
 setwd("C:/Users/43439535/Documents/Lisa/phd/aerial survey/data/speed trials")
 
-lisa  <- read.csv("speed_trials_datasheet_lisa.csv", header = T, skip = 1)
+dat  <- read.csv("speed_trials_datasheet_lisa.csv", header = T, skip = 1)
+
+#install plotly using devtools
+install.packages("viridis") # dependency
+install.packages("devtools")
+devtools::install_github("ropensci/plotly")
 
 library(mapdata)
 library(maps)
@@ -11,15 +16,12 @@ library(maptools)
 library(ggplot2)
 library(plotly)
 
-aust  <- map("world2Hires", regions="Australia", fill = TRUE, col = "grey", xlim = c(110, 160), ylim = c(-45, -10.05140))
+aust  <- map("world2Hires", regions="Australia", fill = TRUE, col = "grey", xlim = c(95, 160), ylim = c(-45, -10.05140))
 aust_poly <- map2SpatialPolygons(aust, IDs = aust$names, proj4string=CRS("+proj=longlat +datum=WGS84"))
 aust_ggplot   <- fortify(aust_poly, region="id") #df for ggplot
 
 
 p <- ggplot() + 
-  geom_polygon(data=aust_ggplot, aes(x=long, y=lat, group=group), color="black", fill = "grey") +
-  geom_point(aes(lisa_points[, 1], lisa_points[, 2], shape = lisa_labels, colour = lisa_labels),
-            position=position_jitter(h=0, w=0)) 
+  geom_polygon(data=aust_ggplot, aes(x=long, y=lat, group=group), color="black", fill = "grey") 
+  #geom_point(aes(dat$longitude, dat$latitude, shape = dat$species, colour = dat$species)) 
 ggplotly(p)
-
-
