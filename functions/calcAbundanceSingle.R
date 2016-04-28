91b9cd9c2a5f7b38483e4f27a796af99bedc9099
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-04-28 13:46:55 +1000

    Changed to 47 flights, but should probably automate this at some point

--- functions/calcAbundanceSingle.R
+++ functions/calcAbundanceSingle.R
@@ -2,7 +2,7 @@ calcAbundanceSingle <- function(area, dataframe, model) {
   
   #calculates density and abundance using distance sampling data
   #area = survey area (km2). Wollongong - Newcastle = 265km
-  #dataframe = total_observations data frame from distanceData function
+  #dataframe = total_observations data frame from createDistanceData function
   #model = ddf model
   
   obs.table <- cbind(rep(1, nrow(dataframe)), dataframe$Trial, dataframe)
@@ -11,7 +11,7 @@ calcAbundanceSingle <- function(area, dataframe, model) {
   region.table <- data.frame(matrix(c(1, area), ncol = 2, byrow = T))
   colnames(region.table) <- c("Region.Label", "Area")
   
-  sample.table <- data.frame(cbind(rep(1, 54), c(1:54), rep(265, 54)))
+  sample.table <- data.frame(cbind(rep(1, 47), c(1:47), rep(265, 47)))
   colnames(sample.table) <- c("Region.Label", "Sample.Label", "Effort")
   
   d <- dht(model, region.table = region.table, sample.table = sample.table, obs.table = obs.table, 
