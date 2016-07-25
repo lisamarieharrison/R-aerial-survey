84bf00a2bcb41f197f49559b29b34895ae7f64df
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-07-25 14:33:26 +1000

    Changed from l_aply to foreach to allow for different parameter values as inputs

--- run_rjmcmc.R
+++ run_rjmcmc.R
@@ -1,11 +1,19 @@
-library(plyr)
 library(doParallel)
+library(foreach)
 
+source("C:/Users/43439535/Documents/Lisa/phd/aerial survey/R/R-aerial-survey/rjmcmc_parallel.R")
+
+#choose number of parallel chains
+n_chains <- 2 
+
+#set up parallel back end
 nodes <- detectCores() - 1
 cl <- makeCluster(nodes)
 registerDoParallel(cl)
 
-l_ply(1:2, runRjmcmc, .parallel = TRUE)
+foreach(chain=1:n_chains, scale0=c(100, 200), shape0=c(1, 10), int0 = c(1, 5)) %dopar% {
+  runRjmcmc(chain, scale0, shape0, int0)
+}
 
 stopCluster(cl)
 
