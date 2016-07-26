8cbca9ae4b5de3b2a50303154bc72ab63046194d
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-07-26 16:13:52 +1000

    Changed scale estimates to be < 10

--- run_rjmcmc.R
+++ run_rjmcmc.R
@@ -15,7 +15,7 @@ nodes <- detectCores() - 1
 cl <- makeCluster(nodes)
 registerDoParallel(cl)
 
-foreach(chain=1:n_chains, scale0=c(100, 200), shape0=c(1, 10), int0 = c(1, 5)) %dopar% {
+foreach(chain=1:n_chains, scale0=c(3, 7), shape0=c(1, 2), int0 = c(1, 2)) %dopar% {
   runRjmcmc(chain, scale0, shape0, int0)
 }
 
