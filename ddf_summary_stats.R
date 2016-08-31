a0802177c1b8b7c57d8f57b1a071da8d11936e1a
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-08-31 14:23:23 +1000

    Script to run ddf, plot fitted detection function and print summary table of mean, se and cv to compare to mcmc

---
+++ ddf_summary_stats.R
@@ -0,0 +1,78 @@
+
+dat <- read.csv("C:/Users/43439535/Documents/Lisa/phd/aerial survey/data/aerial_survey_summary_r.csv", header = T)
+
+if (Sys.info()[4] == "SCI-6246") {
+  setwd(dir = "C:/Users/43439535/Documents/Lisa/phd/aerial survey/data")
+  source_location <- "~/Lisa/phd/aerial survey/R/R-aerial-survey/"
+} else {
+  setwd(dir = "C:/Users/Lisa/Documents/phd/aerial survey/data")
+  source_location <- "~/phd/aerial survey/R/code/R-aerial-survey/"
+}
+
+library(chron)
+library(plyr)
+library(mrds)
+library(Distance)
+
+dat <- read.csv("aerial_survey_summary_r.csv", header = T)
+
+file_list <- c("fun_calculate_envt_effort.R",
+               "functions/createDistanceData.R",
+               "/functions/mrds_modified_functions.R",
+               "functions/calcAbundanceAndCVExtraP.R",
+               "mrds_modified_for_gamma.R")
+
+for (f in file_list) {
+  
+  source(paste0(source_location, f))
+  
+}
+
+
+#remove duplicated observations
+#dat <- dat[!duplicated(dat), ]
+
+#remove secondary observations
+dat <- dat[!dat$Secondary == "Y", ]
+
+#check number of sightings = 2695
+#getting 2375 
+dim(dat[dat$Type == "S", ])
+
+dat$Count <- NA
+dat$Count[dat$Type == "S"] <- 1
+
+dat$Date <- chron(dates. = as.character(dat$Date), format = "d/m/y")
+
+#plot dolphin group size
+hist(dat$Number[dat$Species == "BOT"], main = "Bottlenose Dolphin Group Size", xlab = "Number of dolphins", col = "lightgrey")
+
+#combine all non-hammerhead sharks
+dat$Species[dat$Species %in% c("W", "Wh", "BS")] <- "S"
+
+
+
+lisa_obs <- dat[dat$Observer == "Lisa" & dat$Flight.Direction == "S", ]
+
+#add a unique trial number to each day
+lisa_obs$Trial <- as.numeric(as.factor(as.character(lisa_obs$Date)))
+
+#Bottlenose dolphins
+total_observations <- createDistanceData(species = "BOT", lisa_obs, truncate = 1000, direction = "S")
+det_fun_BOT <- ddf(method = 'ds',dsmodel =~ cds(key = "gamma", formula=~1), data = total_observations, meta.data = list(left = 50, width = 1000))
+plot(det_fun_BOT, main = "Bottlenose Dolphins")
+
+
+est <- exp(det_fun_BOT$par) + c(1, 0)
+sd <- c(summary(det_fun_BOT)$coeff$key.shape[2]$se, summary(det_fun_BOT)$coeff$key.scale[2]$se)
+cv <- sd/est
+
+#summary table
+
+sum_sats <- cbind(est, sd, cv)
+colnames(sum_sats) <- c("Est", "SE", "CV")
+rownames(sum_sats) <- c("Shape", "Scale")
+sum_sats <- round(sum_sats, 4)
+
+sum_sats
+
