d61b896f00afa05a5850a554460b63d37013a927
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-08-31 15:47:59 +1000

    Added /2 correction factors to prevent likelihood from going to infinity when lots of observations and very good model

--- test_likelihood_gamma.R
+++ test_likelihood_gamma.R
@@ -3,13 +3,13 @@ calcLik <- function (x) {
   
   fe <- apply(cd, 1, calcFe, sig.y, sig.s, sig.cc, sig.wc, sha2, x, sig.ss)
   u <- integrate(f.gamma.function, 0, 1000, x, sha2)$value/1000
-  post <- log(prod(fe/u)) 
+  post <- log(prod((fe/u)/2)) 
   
   return (post)
   
 }
 
-liks <- lapply(200:400, calcLik)
+liks <- lapply(100:400, calcLik)
 
-plot(200:400, liks)
+plot(100:400, liks)
 abline(v = 318, col = "red")
