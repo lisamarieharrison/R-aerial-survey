eb0123edc8f4e1fce48e24801fe2ea5edbd8d4e5
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-09-05 14:14:50 +1000

    Added covariates for detection function

--- functions/createDistanceData.R
+++ functions/createDistanceData.R
@@ -17,9 +17,9 @@ createDistanceData <- function(species, lisa_obs, direction, truncate=NULL) {
     lisa_obs <- lisa_obs[lisa_obs$Dist..from.transect <= truncate, ]
   }
   
-  total_observations <- cbind(1:nrow(lisa_obs), rep(1, nrow(lisa_obs)), rep(1, nrow(lisa_obs)), lisa_obs$Dist..from.transect, lisa_obs$Species, lisa_obs$Trial, lisa_obs$Number)
+  total_observations <- cbind(1:nrow(lisa_obs), rep(1, nrow(lisa_obs)), rep(1, nrow(lisa_obs)), lisa_obs$Dist..from.transect, lisa_obs$Species, lisa_obs$Trial, lisa_obs$Number, lisa_obs$Season, lisa_obs$Year, lisa_obs$Beaufort.Sea.State, lisa_obs$Water.clarity, lisa_obs$Cloud.cover)
   total_observations <- data.frame(total_observations)
-  colnames(total_observations) <- c("object", "observer", "detected", "distance", "species", "Trial", "size")
+  colnames(total_observations) <- c("object", "observer", "detected", "distance", "species", "Trial", "size", "Season", "Year", "Sea.state", "Water.clarity", "Cloud.cover")
   
   total_observations <- apply(total_observations, 2, as.character)
   total_observations <- data.frame(apply(total_observations, 2, as.numeric))
@@ -27,7 +27,7 @@ createDistanceData <- function(species, lisa_obs, direction, truncate=NULL) {
   if (species == "BOT") {
     total_observations <- total_observations[!is.na(total_observations$size), ]
   } else {
-    total_observations <- total_observations[, 1:6]
+    total_observations <- total_observations[, c(1:6, 7:ncol(total_observations))]
   }
   
   return(total_observations)
