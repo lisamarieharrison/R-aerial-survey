8798ba7425f1e4c2adf1293f59b4dc1c32fb7fbd
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2017-01-05 14:07:56 +1100

    Removed box around plots to make it more pretty

--- plot_det_fun.R
+++ plot_det_fun.R
@@ -25,9 +25,9 @@ f.gamma.function <- function (dis, key.scale, key.shape) {
 }
 
 par(mfrow = c(1, 3), mar = c(2, 2, 2, 2), oma = c(4, 4, 1, 1))
-plot(f.hn.function(1:1000, sigma = 1000), type = "l", main = "Half-normal", lwd = 2, xlab = "", ylab = "", cex.main = 2)
-plot(f.haz.function(1:1000, sigma = 500, shape = 5), type = "l", main = "Hazard-rate", lwd = 2, xlab = "", ylab = "", cex.main = 2)
-plot(f.gamma.function(1:1000, key.scale = 600, key.shape = 1.2), type = "l", main = "Gamma", lwd = 2, xlab = "", ylab = "", cex.main = 2)
+plot(f.hn.function(1:1000, sigma = 1000), type = "l", main = "Half-normal", lwd = 2, xlab = "", ylab = "", cex.main = 2, bty = "l")
+plot(f.haz.function(1:1000, sigma = 500, shape = 5), type = "l", main = "Hazard-rate", lwd = 2, xlab = "", ylab = "", cex.main = 2, bty = "l")
+plot(f.gamma.function(1:1000, key.scale = 600, key.shape = 1.2), type = "l", main = "Gamma", lwd = 2, xlab = "", ylab = "", cex.main = 2, bty = "l")
 
 mtext("Detection Probability", side = 2, outer = TRUE, line = 2, cex = 2)
 mtext("Distance", side = 1, outer = TRUE, line = 2, cex = 2)
